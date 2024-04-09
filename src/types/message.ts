748804fdab0aaf5d55171defca4bd1c9f1b9ba14
6mn12j <dbwk0404@hanmail.net>
2024-04-10 01:50:53 +0900

    Fix: type error

--- src/types/message.ts
+++ src/types/message.ts
@@ -1,11 +1,20 @@
-type Message = {
-  messageId: number;
-  boxId: number;
-  createdBy: string;
-  tag: MessageTag;
-  contents: string;
-  createdAt: string;
-};
+type Message =
+  | {
+      messageId: number;
+      boxId: number;
+      createdBy: string;
+      tag: MessageTag;
+      contents: string;
+      createdAt: string;
+    }
+  | {
+      messageId?: undefined;
+      boxId?: undefined;
+      createdBy?: undefined;
+      tag?: undefined;
+      contents?: undefined;
+      createdAt?: undefined;
+    };
 
 type MessageTag = 'health' | 'peace' | 'happiness' | 'love';
 export type { Message, MessageTag };
