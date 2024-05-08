955b5d603e0134bccadb01e83e57acb973a03dc6
6mn12j <dbwk0404@hanmail.net>
2024-05-09 00:41:09 +0900

    Fix: S3 업로드 시 public-read 권한 추가

--- src/shared/utils/uploadS3.ts
+++ src/shared/utils/uploadS3.ts
@@ -13,7 +13,7 @@ const accessKeyId = process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID as string;
 const secretAccessKey = process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY as string;
 
 export const uploadImageToS3 = async (props: uploadImageToS3Props) => {
-  const { file } = props;
+  const { file, fileName } = props;
 
   const s3 = new S3Client({
     credentials: {
@@ -25,9 +25,10 @@ export const uploadImageToS3 = async (props: uploadImageToS3Props) => {
 
   const params = {
     Bucket: 'hbdy-s3',
-    Key: file.name,
+    Key: fileName ?? file.name,
     Body: file,
     ContentType: file.type,
+    ACL: 'public-read' as const,
   };
 
   const upload = new Upload({
