a9378b608ebd4175cbe60d5468eee9e492ff821a
6mn12j <dbwk0404@hanmail.net>
2024-06-12 02:01:30 +0900

    fix: 공유하기 iphone alert 안뜨는 현상

--- src/shared/utils/copyClipBoard.ts
+++ src/shared/utils/copyClipBoard.ts
@@ -1,7 +1,7 @@
 export const copyClipBoard = async (text: string) => {
   try {
     await navigator.clipboard.writeText(text);
-    alert('클립보드에 링크가 복사되었어요.');
+    window.alert('클립보드에 링크가 복사되었어요.');
   } catch (err) {
     console.log(err);
   }
